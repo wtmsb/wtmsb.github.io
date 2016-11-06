9a2eb82904919aeb0fb4a264841e1b68786786b2
Yi Zhang <zhayi@bilicraft.moe>
2016-11-06 00:19:50 -0400

    init commit

---
+++ static/index.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#welcome',
+    data: {
+        name: 'wtmsb1111',
+        greetings: 'Make ??? Great Again111111',
+        projects: [
+            {
+                title: 'MugMap',
+                url: 'https://github.com/wtmsb/MugMap',
+                desc: 'Map Visualization of MUG Players Distribution'
+            },
+            {
+                title: '',
+                url: '#',
+                desc: 'To be continued...'
+            }
+        ]
+    },
+
+    methods: {}
+});
